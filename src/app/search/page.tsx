a95e74ed8c7c4152bffebb15bf9309d950a110de
RandyLiu6410 <47877698+RandyLiu6410@users.noreply.github.com>
2024-08-13 00:55:07 +0800

    feat: Init project (#2)
    * feat: Init project
    
    * feat: add typescript check
    
    * feat: env config
    
    * feat: env config

---
+++ src/app/search/page.tsx
@@ -0,0 +1,16 @@
+/**
+ * Demo Zustand Store
+ */
+
+import SearchBar from '@/common/components/elements/SearchBar'
+import ResultBlock from '@/common/components/elements/SearchBar/ResultBlock'
+
+export default function SearchPage () {
+  return (
+    <div>
+      <h1>Search Page</h1>
+      <SearchBar />
+      <ResultBlock />
+    </div>
+  )
+}
