e6759fd277dafcb7f428a061547a2118eb58a93e
Randy Liu <wert6410@gmail.com>
2024-08-12 23:52:41 +0800

    feat: Init i18n

---
+++ src/app/[lang]/sample/page.tsx
@@ -0,0 +1,25 @@
+import { useTranslation } from '@/common/lib/i18n/serverHooks'
+import { Language } from '@/common/lib/i18n/types'
+import Link from 'next/link'
+
+interface SamplePageParams {
+  lang: Language;
+}
+
+export default async function SamplePage ({
+  params: { lang },
+}: {
+  params: SamplePageParams;
+}) {
+  const { t } = await useTranslation(lang)
+
+  return (
+    <>
+      <h1>Hi from sample page!</h1>
+      <Link href={`/${lang}`}>back</Link>
+
+      {/** 測試 server render */}
+      <p>Sample: {t('sample')}</p>
+    </>
+  )
+}
---
+++ src/common/lib/i18n/settings.ts
@@ -0,0 +1,21 @@
+import { InitOptions } from 'i18next'
+import { Language } from './types'
+
+export const fallbackLang: Language = 'en-US'
+export const languages: Array<Language> = [fallbackLang, 'zh-TW']
+export const defaultNamespace = 'translation'
+export const cookieName = 'i18next'
+
+export function getOptions (lang = fallbackLang, ns = defaultNamespace): InitOptions {
+  /**
+   * 相關 config 參考: https://www.i18next.com/overview/configuration-options
+   */
+  return {
+    supportedLngs: languages,
+    fallbackLng: fallbackLang,
+    lng: lang,
+    fallbackNS: defaultNamespace,
+    defaultNS: defaultNamespace,
+    ns,
+  }
+}
