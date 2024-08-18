aa534ed205be4cafeecdea038fa16f1157608366
RandyLiu6410 <47877698+RandyLiu6410@users.noreply.github.com>
2024-08-18 11:22:41 +0800

    feat: atomic general button (#6)
    * feat: icon init
    
    * feat: general button

---
+++ src/common/components/atoms/UButton.tsx
@@ -0,0 +1,24 @@
+'use client'
+
+import { Button, ButtonProps } from '@mui/material'
+import { styled } from '@mui/material/styles'
+import type { ComponentType } from 'react'
+
+interface UButtonProps extends ButtonProps {
+  rounded?: boolean;
+}
+
+const UButton = styled(Button)<UButtonProps>(
+  ({ theme, rounded, color, disabled }) => ({
+    ...(rounded && {
+      borderRadius: '50px',
+    }),
+    textTransform: 'none', // 消除文字大寫
+    padding: `${theme.spacing(1)} ${theme.spacing(3)}`,
+    ...((color === 'primary' && disabled) && {
+      backgroundColor: `${theme.palette.primary.main} !important`,
+    }),
+  })
+) as ComponentType<UButtonProps>
+
+export default UButton
