45b76c85b37b4304b83a84273a8eb480347f6493
RandyLiu6410 <47877698+RandyLiu6410@users.noreply.github.com>
2024-08-26 22:52:22 +0800

    feat: carousel & kv card init (#12)
    * feat: carousel & kv card init
    
    * fix: remove redundant codes

--- src/app/[lang]/page.tsx
+++ src/app/[lang]/page.tsx
@@ -1,31 +1,14 @@
 import * as React from 'react'
 import Container from '@mui/material/Container'
-import Typography from '@mui/material/Typography'
-import Box from '@mui/material/Box'
-import { Button } from '@mui/material'
+import IndexKvCards from '@/common/components/elements/IndexKvCards'
+import { Stack } from '@mui/material'
 
 export default function Home () {
   return (
     <Container maxWidth="lg">
-      <Box
-        sx={{
-          my: 4,
-          display: 'flex',
-          flexDirection: 'column',
-          justifyContent: 'center',
-          alignItems: 'center',
-        }}
-      >
-        <Typography variant="h4" component="h1" sx={{ mb: 2 }}>
-          Material UI - Next.js App Router example in TypeScript
-        </Typography>
-        <Button variant="contained" color="primary">Primary</Button>
-        <Button variant="contained" color="secondary">Secondary</Button>
-        <Button variant="contained" color="success">Success</Button>
-        <Button variant="contained" color="error">Error</Button>
-        <Button variant="contained" color="warning">Warning</Button>
-        <Button variant="contained" color="info">Info</Button>
-      </Box>
+      <Stack direction="column" gap={10} alignContent="center" justifyContent="center">
+        <IndexKvCards />
+      </Stack>
     </Container>
   )
 }
