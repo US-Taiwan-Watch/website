a2088c616fc39beccda3faf446f26fdfa39895d9
RandyLiu6410 <47877698+RandyLiu6410@users.noreply.github.com>
2024-09-12 21:25:19 +0800

    feat: people landing page (#21)

--- src/common/lib/mui/themeProvider.tsx
+++ src/common/lib/mui/themeProvider.tsx
@@ -2,18 +2,20 @@
 
 import { ThemeProvider as MuiThemeProvider } from '@mui/material/styles'
 import { Language } from '@/common/lib/i18n/types'
-import { createUSTWTheme } from '@/common/lib/mui/theme'
+import { createUSTWTheme, CreateUSTWThemeOverride } from '@/common/lib/mui/theme'
 import type React from 'react'
 
 const ThemeProvider = ({
   lang,
+  override,
   children,
 }: {
   lang: Language;
+  override?: CreateUSTWThemeOverride
   children: React.ReactNode;
 }) => {
   return (
-    <MuiThemeProvider theme={createUSTWTheme('light', lang)}>
+    <MuiThemeProvider theme={createUSTWTheme('light', lang, override)}>
       {children}
     </MuiThemeProvider>
   )
