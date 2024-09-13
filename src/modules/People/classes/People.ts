03a5b7c477c9f3a2b028ebdf67f49e2c23101207
RandyLiu6410 <wert6410@gmail.com>
2024-09-14 01:00:34 +0800

    feat: people experience

--- src/modules/People/classes/People.ts
+++ src/modules/People/classes/People.ts
@@ -4,6 +4,34 @@ import { PeoplePosition } from '@/modules/People/enums/PeoplePosition'
 import dayjs, { Dayjs } from 'dayjs'
 import { isArray, isString } from 'lodash-es'
 
+interface PartyExperienceArgs {
+  party: Party
+  start?: string
+  end?: string
+}
+export interface PartyExperience {
+  party: Party
+  start?: Dayjs
+  end?: Dayjs
+}
+
+interface ExperienceArgs {
+  title: string
+  subtitle?: string
+  start?: string
+  end?: string
+  descriptions?: Array<string>
+  experience?: Array<ExperienceArgs>
+}
+export interface Experience {
+  title: string
+  subtitle?: string
+  start?: Dayjs
+  end?: Dayjs
+  descriptions?: Array<string>
+  experience?: Array<Experience>
+}
+
 interface PeopleArgs {
   id: string
   name: string
@@ -13,11 +41,8 @@ interface PeopleArgs {
   position: PeoplePosition
   congress: Congress
   tags: Array<string>
-  partyExperience: Array<{
-    party: Party
-    start?: string
-    end?: string
-  }>
+  partyExperience: Array<PartyExperienceArgs>
+  experience: Array<ExperienceArgs>
 }
 
 export class People {
@@ -38,11 +63,9 @@ export class People {
   // 標籤
   tags?: Array<string>
   // TODO: 政黨經歷暫定，後續討論
-  partyExperience?: Array<{
-    party: Party
-    start?: Dayjs
-    end?: Dayjs
-  }>
+  partyExperience?: Array<PartyExperience>
+  // TODO: 經歷暫定，後續討論
+  experience?: Array<Experience>
 
   constructor(private readonly people: PeopleArgs) {
     if (isString(people.id)) {
@@ -70,25 +93,73 @@ export class People {
       this.tags = people.tags
     }
     if (isArray(people.partyExperience)) {
-      this.partyExperience = people.partyExperience.map((item) => {
-        return {
-          party: item.party,
-          start: item.start
-            ? dayjs(item.start).isValid()
-              ? dayjs(item.start)
-              : undefined
-            : undefined,
-          end: item.end
-            ? dayjs(item.end).isValid()
-              ? dayjs(item.end)
-              : undefined
-            : undefined,
-        }
-      })
+      this.partyExperience = People.TransformPartyExperience(
+        people.partyExperience
+      )
+    }
+    if (isArray(people.experience)) {
+      this.experience = People.TransformExperience(people.experience)
     }
   }
 
   get link() {
     return `/people/${this.id}`
   }
+
+  static TransformPartyExperience(partyExperience: Array<PartyExperienceArgs>) {
+    return partyExperience.map((item: PartyExperienceArgs): PartyExperience => {
+      return {
+        party: item.party,
+        start: item.start
+          ? dayjs(item.start).isValid()
+            ? dayjs(item.start)
+            : undefined
+          : undefined,
+        end: item.end
+          ? dayjs(item.end).isValid()
+            ? dayjs(item.end)
+            : undefined
+          : undefined,
+      }
+    })
+  }
+
+  static TransformExperience(experience: Array<ExperienceArgs>) {
+    return experience.map((item: ExperienceArgs): Experience => {
+      return {
+        title: item.title,
+        subtitle: item.subtitle,
+        start: item.start
+          ? dayjs(item.start).isValid()
+            ? dayjs(item.start)
+            : undefined
+          : undefined,
+        end: item.end
+          ? dayjs(item.end).isValid()
+            ? dayjs(item.end)
+            : undefined
+          : undefined,
+        descriptions: item.descriptions,
+        experience: item.experience
+          ? People.TransformExperience(item.experience)
+          : undefined,
+      }
+    })
+  }
+
+  /**
+   * Calculate two dayjs instances to years and months
+   * e.x. 2022-01-01 ~ 2023-06-01 => 1 yr 5 mo
+   * @param experience
+   * @returns
+   */
+  static CalculateExperienceDuration(experience: Experience) {
+    return {
+      year: experience.end?.diff(experience.start, 'year') ?? 0,
+      month: (experience.end?.diff(experience.start, 'month') ?? 0) % 12,
+    }
+  }
+
+  // Nov 2022
+  static TimeFormat = 'MMM YYYY'
 }
--- src/modules/People/components/PeopleTracker/CardContent/Experience.tsx
+++ src/modules/People/components/PeopleTracker/CardContent/Experience.tsx
@@ -1,12 +1,202 @@
 import UCardHeader from '@/common/components/atoms/UCardHeader'
 import { BriefcaseIcon } from '@/common/styles/assets/Icons'
-import { CardContent, useTheme } from '@mui/material'
+import {
+  Box,
+  Card,
+  CardContent,
+  Dialog,
+  Stack,
+  Typography,
+  useTheme,
+} from '@mui/material'
 import ArrowForwardIcon from '@mui/icons-material/ArrowForward'
 import UIconButton from '@/common/components/atoms/UIconButton'
 import { USTWTheme } from '@/common/lib/mui/theme'
+import CloseIcon from '@mui/icons-material/Close'
+import { useMemo, useState } from 'react'
+import {
+  type Experience as PeopleExperience,
+  People,
+} from '@/modules/People/classes/People'
+import Timeline from '@mui/lab/Timeline'
+import TimelineItem, { timelineItemClasses } from '@mui/lab/TimelineItem'
+import TimelineSeparator from '@mui/lab/TimelineSeparator'
+import TimelineConnector from '@mui/lab/TimelineConnector'
+import TimelineContent from '@mui/lab/TimelineContent'
+import TimelineDot from '@mui/lab/TimelineDot'
 
-const Experience = function Experience() {
+const useExperienceTime = function (experience: PeopleExperience) {
+  // TODO: i18n
+  const durationText = useMemo(() => {
+    const duration = People.CalculateExperienceDuration(experience)
+    let text = ''
+    if (duration.year > 0) {
+      text += `${duration.year} yr${duration.year > 1 ? 's' : ''} `
+    }
+    if (duration.month > 0) {
+      text += `${duration.month} mo${duration.month > 1 ? 's' : ''}`
+    }
+    return text
+  }, [experience])
+
+  // TODO: i18n
+  const timeText = useMemo(() => {
+    if (!experience.start) return ''
+
+    // 現在進行中
+    if (!experience.end) {
+      return `${experience.start.format(People.TimeFormat)} ~ Present`
+    } else if (experience.experience) {
+      return durationText
+    } else {
+      return `${experience.start.format(People.TimeFormat)} ~ ${experience.end.format(People.TimeFormat)} • ${durationText}`
+    }
+  }, [experience, durationText])
+
+  return { timeText, durationText }
+}
+
+const ExperienceTimelineItem = function ExperienceTimelineItem({
+  experience,
+  isLast,
+}: {
+  experience: PeopleExperience
+  isLast?: boolean
+}) {
   const theme = useTheme<USTWTheme>()
+  const { timeText } = useExperienceTime(experience)
+
+  return (
+    <TimelineItem
+      sx={{
+        minHeight: 'unset',
+      }}
+    >
+      <TimelineSeparator>
+        <TimelineDot />
+        {!isLast && <TimelineConnector />}
+      </TimelineSeparator>
+      <TimelineContent>
+        <Typography variant="bodyM" fontWeight={700}>
+          {experience.title}
+        </Typography>
+        <Typography
+          variant="bodyS"
+          sx={{ color: theme.color.neutral[500] }}
+          fontWeight={400}
+        >
+          {timeText}
+        </Typography>
+      </TimelineContent>
+    </TimelineItem>
+  )
+}
+const ExperienceTimeline = function ExperienceTimeline({
+  experience,
+}: {
+  experience: Array<PeopleExperience>
+}) {
+  const theme = useTheme<USTWTheme>()
+
+  return (
+    <Timeline
+      sx={{
+        padding: 0,
+        margin: 0,
+        [`& .${timelineItemClasses.root}:before`]: {
+          flex: 0,
+          padding: 0,
+        },
+        [`& .MuiTimelineConnector-root`]: {
+          margin: '-15.5px 0',
+          backgroundColor: theme.color.grey[100],
+          width: '12px',
+        },
+        [`& .MuiTimelineDot-root`]: {
+          zIndex: 1,
+          backgroundColor: theme.color.grey[400],
+        },
+      }}
+    >
+      {experience.map((exp, index) => (
+        <ExperienceTimelineItem
+          key={index}
+          experience={exp}
+          isLast={index === experience.length - 1}
+        />
+      ))}
+    </Timeline>
+  )
+}
+
+const ExperienceRow = function ExperienceRow({
+  experience,
+}: {
+  experience: PeopleExperience
+}) {
+  const theme = useTheme<USTWTheme>()
+  const { timeText } = useExperienceTime(experience)
+
+  return (
+    <Stack className="experience-row" padding={1}>
+      <Typography variant="bodyM" fontWeight={700}>
+        {experience.title}
+      </Typography>
+      {experience.subtitle && (
+        <Typography variant="bodyS" fontWeight={500}>
+          {experience.subtitle}
+        </Typography>
+      )}
+      <Typography
+        variant="bodyS"
+        sx={{ color: theme.color.neutral[500] }}
+        fontWeight={400}
+      >
+        {timeText}
+      </Typography>
+      {experience.descriptions &&
+        experience.descriptions.map((description, index) => (
+          <Typography
+            key={index}
+            variant="bodyS"
+            fontWeight={500}
+            sx={{ color: theme.color.neutral[500] }}
+          >
+            {description}
+          </Typography>
+        ))}
+      {experience.experience && (
+        <ExperienceTimeline experience={experience.experience} />
+      )}
+    </Stack>
+  )
+}
+
+interface ExperienceProps {
+  experience: PeopleExperience[]
+  isModal?: boolean
+  onActionClick?: () => void
+}
+
+const Experience = function Experience({
+  experience,
+  isModal,
+  onActionClick,
+}: ExperienceProps) {
+  const theme = useTheme<USTWTheme>()
+  const [isModalOpen, setIsModalOpen] = useState(false)
+
+  const handleActionClick = () => {
+    if (!isModal) {
+      setIsModalOpen(true)
+    } else {
+      onActionClick?.()
+    }
+  }
+
+  const handleCloseModal = () => {
+    setIsModalOpen(false)
+  }
 
   return (
     <>
@@ -15,12 +205,69 @@ const Experience = function Experience() {
         icon={<BriefcaseIcon />}
         iconColor="primary"
         action={
-          <UIconButton variant="rounded" color="inherit" size="small">
-            <ArrowForwardIcon sx={{ color: theme.color.neutral[500] }} />
+          <UIconButton
+            variant="rounded"
+            color="inherit"
+            size="small"
+            onClick={handleActionClick}
+          >
+            {isModal ? (
+              <CloseIcon sx={{ color: theme.color.neutral[500] }} />
+            ) : (
+              <ArrowForwardIcon sx={{ color: theme.color.neutral[500] }} />
+            )}
           </UIconButton>
         }
       />
-      <CardContent></CardContent>
+      <CardContent
+        sx={{
+          padding: 0,
+          position: isModal ? 'relative' : 'absolute',
+          width: isModal ? '100%' : 'calc(100% - 48px)',
+          [`& .experience-row`]: {
+            '&:not(:last-child)': {
+              borderBottom: `1px solid ${theme.color.grey[1900]}`,
+            },
+          },
+        }}
+      >
+        {experience?.map((exp, index) => (
+          <ExperienceRow key={index} experience={exp} />
+        ))}
+      </CardContent>
+      {isModalOpen && (
+        <Dialog
+          open={isModalOpen}
+          onClose={handleCloseModal}
+          PaperProps={{
+            sx: {
+              width: '100%',
+              borderRadius: theme.shape.borderRadius,
+            },
+          }}
+        >
+          <Box
+            sx={{
+              bgcolor: 'background.paper',
+            }}
+          >
+            <Card
+              sx={{
+                padding: 2,
+                '& .MuiCardContent-root': {
+                  paddingBottom: 0,
+                },
+              }}
+            >
+              <Experience
+                isModal
+                onActionClick={handleCloseModal}
+                experience={experience}
+              />
+            </Card>
+          </Box>
+        </Dialog>
+      )}
     </>
   )
 }
