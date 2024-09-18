f7dbd696663e0a408ae53a59cbcbc7e934ad40fc
RandyLiu6410 <wert6410@gmail.com>
2024-09-19 00:14:53 +0800

    feat: 首頁 opinion articles

--- src/common/lib/zustand/hooks/useOpinionStore.ts
+++ src/common/lib/zustand/hooks/useOpinionStore.ts
@@ -2,12 +2,15 @@ import { config } from '@/config'
 import OpinionCategory from '@/modules/Opinion/classes/OpinionCategory'
 import {
   highlightedOpinionCategories,
+  homeOpinionCategories,
   opinionCategories,
 } from '@/modules/Opinion/data'
 import { mountStoreDevtool } from 'simple-zustand-devtools'
 import { create } from 'zustand'
 
 type State = {
+  /** 首頁分類 */
+  homeCategories: Array<OpinionCategory>
   /** 熱門分類，存在在 NavBar 中 */
   highlightedCategories: Array<OpinionCategory>
   /** 所有分類 */
@@ -15,18 +18,28 @@ type State = {
 }
 
 type Action = {
+  fetchHomeCategories: () => void
   fetchCategories: () => void
   fetchHighlightedCategories: () => void
   clearCategories: () => void
 }
 
 const initialState: State = {
+  homeCategories: [],
   highlightedCategories: [],
   categories: [],
 }
 
 const useOpinionStore = create<State & Action>((set) => ({
   ...initialState,
+  fetchHomeCategories: () => {
+    // TODO: 從 API 取得資料
+    set(() => ({
+      homeCategories: homeOpinionCategories.map(
+        (category) => new OpinionCategory(category)
+      ),
+    }))
+  },
   fetchHighlightedCategories: () => {
     // TODO: 從 API 取得資料
     set(() => ({
