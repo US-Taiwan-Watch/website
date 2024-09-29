c24e2f9470f35e63ac0b6c8605fbc2bd689d9153
林子皓 <>
2024-09-29 18:45:12 +0800

    feat: bill filter UI

---
+++ src/common/components/elements/BillFilter/index.tsx
@@ -0,0 +1,176 @@
+'use client'
+
+import { Select, MenuItem, useTheme, Divider } from '@mui/material'
+import UHStack from '@/common/components/atoms/UHStack'
+import { styled, USTWTheme } from '@/common/lib/mui/theme'
+import KeyboardArrowDownOutlinedIcon from '@mui/icons-material/KeyboardArrowDownOutlined'
+import UButton from '@/common/components/atoms/UButton'
+import { BillFilterState } from '@/common/components/elements/BillFilter/useBillFilter'
+
+const StyledFilterContainer = styled(UHStack)(({ theme }) => ({
+  padding: theme.spacing(1.5, 2),
+  justifyContent: 'space-between',
+  alignItems: 'center',
+  spacing: 1,
+  width: '100%',
+  backgroundColor: theme.color.common.white,
+  borderRadius: '15px',
+}))
+
+const StyledSelect = styled(Select)(({ theme }) => ({
+  borderRadius: '9px',
+  border: `1px solid ${theme.color.grey[1400]}`,
+  backgroundColor: theme.color.grey[2600],
+  padding: theme.spacing(1, 1.75),
+  '& .MuiInputBase-input': {
+    padding: 0,
+    fontSize: 14,
+    fontWeight: 500,
+  },
+  '& .MuiOutlinedInput-notchedOutline': {
+    border: 'none',
+  },
+  '& .MuiSelect-icon': {
+    color: theme.color.grey[2700],
+  },
+}))
+
+type BillFilterProps = {
+  filterState: BillFilterState
+  handleFilterChange: (field: keyof BillFilterState, value: string) => void
+  handleReset: () => void
+}
+
+export default function BillFilter({
+  filterState,
+  handleFilterChange,
+  handleReset,
+}: BillFilterProps) {
+  const theme = useTheme<USTWTheme>()
+
+  return (
+    <StyledFilterContainer>
+      <UHStack spacing={1.5}>
+        <StyledSelect
+          value={filterState.category}
+          onChange={(e) =>
+            handleFilterChange('category', String(e.target.value))
+          }
+          displayEmpty
+          IconComponent={KeyboardArrowDownOutlinedIcon}
+          sx={{
+            backgroundColor: theme.color.purple[100],
+            borderColor: theme.color.purple[200],
+            '& .MuiSelect-icon': {
+              color: theme.color.neutral[500],
+            },
+          }}
+        >
+          <MenuItem value="" disabled>
+            Category
+          </MenuItem>
+          <MenuItem value="A">A</MenuItem>
+          <MenuItem value="B">B</MenuItem>
+          <MenuItem value="C">C</MenuItem>
+        </StyledSelect>
+
+        <Divider
+          orientation="vertical"
+          flexItem
+          sx={{
+            borderColor: theme.color.neutral[200],
+          }}
+        />
+
+        <StyledSelect
+          value={filterState.party}
+          onChange={(e) => handleFilterChange('party', String(e.target.value))}
+          displayEmpty
+          disabled={!filterState.category}
+          IconComponent={KeyboardArrowDownOutlinedIcon}
+        >
+          <MenuItem value="" disabled>
+            Party
+          </MenuItem>
+          <MenuItem value="A">A</MenuItem>
+          <MenuItem value="B">B</MenuItem>
+          <MenuItem value="C">C</MenuItem>
+        </StyledSelect>
+
+        <StyledSelect
+          value={filterState.type}
+          onChange={(e) => handleFilterChange('type', String(e.target.value))}
+          displayEmpty
+          disabled={!filterState.category}
+          IconComponent={KeyboardArrowDownOutlinedIcon}
+        >
+          <MenuItem value="" disabled>
+            Type
+          </MenuItem>
+          <MenuItem value="A">A</MenuItem>
+          <MenuItem value="B">B</MenuItem>
+          <MenuItem value="C">C</MenuItem>
+        </StyledSelect>
+
+        <StyledSelect
+          value={filterState.status}
+          onChange={(e) => handleFilterChange('status', String(e.target.value))}
+          displayEmpty
+          disabled={!filterState.category}
+          IconComponent={KeyboardArrowDownOutlinedIcon}
+        >
+          <MenuItem value="" disabled>
+            Status
+          </MenuItem>
+          <MenuItem value="A">A</MenuItem>
+          <MenuItem value="B">B</MenuItem>
+          <MenuItem value="C">C</MenuItem>
+        </StyledSelect>
+
+        <StyledSelect
+          value={filterState.sponsors}
+          onChange={(e) =>
+            handleFilterChange('sponsors', String(e.target.value))
+          }
+          displayEmpty
+          disabled={!filterState.category}
+          IconComponent={KeyboardArrowDownOutlinedIcon}
+        >
+          <MenuItem value="" disabled>
+            Sponsors
+          </MenuItem>
+          <MenuItem value="A">A</MenuItem>
+          <MenuItem value="B">B</MenuItem>
+          <MenuItem value="C">C</MenuItem>
+        </StyledSelect>
+
+        <StyledSelect
+          value={filterState.cosponsors}
+          onChange={(e) =>
+            handleFilterChange('cosponsors', String(e.target.value))
+          }
+          displayEmpty
+          disabled={!filterState.category}
+          IconComponent={KeyboardArrowDownOutlinedIcon}
+        >
+          <MenuItem value="" disabled>
+            Cosponsors
+          </MenuItem>
+          <MenuItem value="A">A</MenuItem>
+          <MenuItem value="B">B</MenuItem>
+          <MenuItem value="C">C</MenuItem>
+        </StyledSelect>
+      </UHStack>
+
+      <UButton
+        variant="contained"
+        color="info"
+        rounded
+        size="large"
+        onClick={handleReset}
+      >
+        Reset
+      </UButton>
+    </StyledFilterContainer>
+  )
+}
--- src/modules/Bill/components/BillList.tsx
+++ src/modules/Bill/components/BillList.tsx
@@ -1,17 +1,28 @@
 'use client'
 
 import UPagination from '@/common/components/atoms/UPagination'
+import BillFilter from '@/common/components/elements/BillFilter'
+import useBillFilter from '@/common/components/elements/BillFilter/useBillFilter'
 import BillCard from '@/modules/Bill/components/BillCard'
 import { BILL_DATA_MOCK } from '@/modules/Bill/data'
 import { Stack } from '@mui/material'
 
 export default function BillList() {
+  const { filterState, handleFilterChange, handleReset } = useBillFilter()
+
   return (
     <Stack gap={7} alignItems="center" pb={10}>
-      <Stack gap={2}>
-        {BILL_DATA_MOCK.map((bill, index) => (
-          <BillCard key={index} mode="horizontal" bill={bill} />
-        ))}
+      <Stack gap={5} alignItems="center">
+        <BillFilter
+          filterState={filterState}
+          handleFilterChange={handleFilterChange}
+          handleReset={handleReset}
+        />
+        <Stack gap={2}>
+          {BILL_DATA_MOCK.map((bill, index) => (
+            <BillCard key={index} mode="horizontal" bill={bill} />
+          ))}
+        </Stack>
       </Stack>
       <UPagination count={10} page={1} onChange={() => {}} />
     </Stack>
