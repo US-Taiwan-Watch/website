2928a3e862915698dcb30da117a08806b52532c7
林子皓 <>
2024-10-03 20:52:57 +0800

    feat: UCardInfo

---
+++ src/common/components/atoms/UCardInfo.tsx
@@ -0,0 +1,38 @@
+'use client'
+
+import ErrorOutlineOutlinedIcon from '@mui/icons-material/ErrorOutlineOutlined'
+import {
+  IconButton,
+  SvgIconProps,
+  Tooltip,
+  TooltipProps,
+  useTheme,
+} from '@mui/material'
+import { USTWTheme } from '@/common/lib/mui/theme'
+
+type UTooltipInfoIconProps = {
+  content: string
+  tooltipProps?: TooltipProps
+  iconProps?: SvgIconProps
+  onClick?: () => void
+}
+
+export default function UCardInfo({
+  content,
+  tooltipProps,
+  iconProps,
+  onClick,
+}: UTooltipInfoIconProps) {
+  const theme = useTheme<USTWTheme>()
+
+  return (
+    <Tooltip title={content} arrow {...tooltipProps}>
+      <IconButton size="small" onClick={onClick}>
+        <ErrorOutlineOutlinedIcon
+          {...iconProps}
+          sx={{ color: theme.color.grey[1800], ...iconProps?.sx }}
+        />
+      </IconButton>
+    </Tooltip>
+  )
+}
