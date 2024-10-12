bd21ba6c53d8da8354e102e63326736fb96f5fe4
Zi-Hao Lin <49062749+I-am-ZiHao@users.noreply.github.com>
2024-10-13 00:05:14 +0800

    feat: UCardInfo (#49)
    * feat: UCardInfo
    
    * fix: rename type
    
    ---------
    
    Co-authored-by: 林子皓 <>

--- src/modules/Bill/components/SingleBill/BillTracker.tsx
+++ src/modules/Bill/components/SingleBill/BillTracker.tsx
@@ -1,21 +1,17 @@
 'use client'
 import UContentCard from '@/common/components/atoms/UContentCard'
 import { TrackerIcon } from '@/common/styles/assets/Icons'
-import { Box, CardContent, useTheme } from '@mui/material'
-import InfoOutlinedIcon from '@mui/icons-material/InfoOutlined'
-import UIconButton from '@/common/components/atoms/UIconButton'
-import { USTWTheme } from '@/common/lib/mui/theme'
+import { Box, CardContent } from '@mui/material'
 import UTimeline from '@/common/components/atoms/UTimeline'
 import { billStatusList } from '@/modules/Bill/constants'
 import { Bill } from '@/modules/Bill/classes/Bill'
+import UCardInfo from '@/common/components/atoms/UCardInfo'
 
 type Props = {
   bill: Bill
 }
 
 export default function BillTracker({ bill }: Props) {
-  const theme = useTheme<USTWTheme>()
-
   return (
     <UContentCard
       withHeader
@@ -23,11 +19,7 @@ export default function BillTracker({ bill }: Props) {
         title: 'Tracker',
         icon: <TrackerIcon />,
         iconColor: 'primary',
-        action: (
-          <UIconButton variant="rounded" color="inherit" size="small">
-            <InfoOutlinedIcon sx={{ color: theme.color.grey[1800] }} />
-          </UIconButton>
-        ),
+        action: <UCardInfo content="Tracker" />,
       }}
     >
       <CardContent>
