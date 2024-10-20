76ca826ee7fd3f9e4700a4eff173d03e53482b22
RandyLiu6410 <wert6410@gmail.com>
2024-10-21 00:10:11 +0800

    feat: divider

--- src/modules/People/components/PeopleTracker/CardContent/CoSponsored.tsx
+++ src/modules/People/components/PeopleTracker/CardContent/CoSponsored.tsx
@@ -2,6 +2,7 @@ import { PeopleJoinIcon } from '@/common/styles/assets/Icons'
 import NumberCard from '@/modules/People/components/PeopleTracker/CardContent/NumberCard'
 import { BILL_DATA_MOCK } from '@/modules/Bill/data'
 import BillCard from '@/modules/Bill/components/BillCard'
+import { Box } from '@mui/material'
 
 const CoSponsored = function () {
   return (
@@ -15,7 +16,15 @@ const CoSponsored = function () {
       }}
     >
       {BILL_DATA_MOCK.map((bill, index) => (
-        <BillCard key={index} mode="horizontal" bill={bill} />
+        <Box
+          key={index}
+          sx={{
+            marginLeft: '8px',
+            marginRight: '8px',
+          }}
+        >
+          <BillCard mode="horizontal" bill={bill} />
+        </Box>
       ))}
     </NumberCard>
   )
--- src/modules/People/components/PeopleTracker/CardContent/Sponsored.tsx
+++ src/modules/People/components/PeopleTracker/CardContent/Sponsored.tsx
@@ -2,6 +2,7 @@ import NumberCard from '@/modules/People/components/PeopleTracker/CardContent/Nu
 import { BILL_DATA_MOCK } from '@/modules/Bill/data'
 import BillCard from '@/modules/Bill/components/BillCard'
 import { Person2Icon } from '@/common/styles/assets/Icons'
+import { Box } from '@mui/material'
 
 const Sponsored = function () {
   return (
@@ -15,7 +16,15 @@ const Sponsored = function () {
       }}
     >
       {BILL_DATA_MOCK.map((bill, index) => (
-        <BillCard key={index} mode="horizontal" bill={bill} />
+        <Box
+          key={index}
+          sx={{
+            marginLeft: '8px',
+            marginRight: '8px',
+          }}
+        >
+          <BillCard mode="horizontal" bill={bill} />
+        </Box>
       ))}
     </NumberCard>
   )
