c0d7ab69e49033ba490352cec85323764377ca45
RandyLiu6410 <wert6410@gmail.com>
2024-11-06 00:39:36 +0800

    fix: remove redundant codes

--- src/common/lib/mui/theme.ts
+++ src/common/lib/mui/theme.ts
@@ -384,15 +384,6 @@ const _lightTheme: USTWThemeOptions = {
         },
       },
     },
-    // MuiInputLabel: {
-    //   styleOverrides: {
-    //     root: {
-    //       '&.Mui-focused': {
-    //         color: 'rgba(0, 0, 0, 0.6)', // 預設 MUI 的 Label 顏色
-    //       },
-    //     },
-    //   },
-    // },
   },
 }
 
