8760aec6bcaef0ea5b2415cdc19b8f2d03e9bcfd
RandyLiu6410 <wert6410@gmail.com>
2024-12-16 23:12:25 +0800

    todo

--- src/app/[lang]/people/layout.tsx
+++ src/app/[lang]/people/layout.tsx
@@ -6,6 +6,7 @@ import { AppRouterCacheProvider } from '@mui/material-nextjs/v13-appRouter'
 import CssBaseline from '@mui/material/CssBaseline'
 import Container from '@mui/material/Container'
 
+// TODO: 確認資料來源
 export const metadata: Metadata = {
   title: 'People',
   description: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.`,
