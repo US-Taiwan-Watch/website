1df527cca6264edde4687621ef82d92597a951b5
RandyLiu6410 <wert6410@gmail.com>
2024-12-20 17:41:48 +0800

    feat: Landing opinion limit and sort

--- src/modules/LandingPage/components/ArticleSection.tsx
+++ src/modules/LandingPage/components/ArticleSection.tsx
@@ -7,51 +7,56 @@ import LandingSectionWrapper from '@/common/components/elements/Landing/LandingS
 import { SectionTitleWithLink } from '@/common/components/elements/Landing/SectionTitle'
 import { OVERLAPPED_SECTION_PADDING_BOTTOM } from '@/modules/LandingPage/constants'
 import useOpinionStore from '@/common/lib/zustand/hooks/useOpinionStore'
-import { useEffect, useState } from 'react'
+import { useState } from 'react'
 import OpinionPostCards from '@/modules/Opinion/components/OpinionPostCards'
 import useOpinionIndex from '@/modules/Opinion/hooks/useOpinionIndex'
 import { ROUTES } from '@/routes'
 import CommonUtils from '@/modules/Common/Common.utils'
 import { useParams } from 'next/navigation'
 import { Language } from '@/common/lib/i18n/types'
+import CategoryProvider from '@/modules/Opinion/providers/CategoryProvider'
 
 const ArticleSection = () => {
   const { lang } = useParams<{ lang: Language }>()
   const [activeCategoryId, setActiveCategoryId] = useState<string | undefined>()
   const landingTags = useOpinionStore((state) => state.landingTags)
 
-  // 預設塞第一個
-  useEffect(() => {
-    if (landingTags.length > 0) {
-      setActiveCategoryId(landingTags[0]?.id ?? undefined)
-    }
-  }, [landingTags])
-
-  const { opinions } = useOpinionIndex(activeCategoryId)
+  const { opinions } = useOpinionIndex(activeCategoryId, 3)
 
   return (
-    <LandingSectionWrapper
-      contentWrapperSx={{
-        paddingBottom: `${OVERLAPPED_SECTION_PADDING_BOTTOM}px`,
-      }}
-    >
-      <SectionTitleWithLink title="Articles" link={ROUTES.OPINION} />
-      <Stack gap={5}>
-        <UHStack gap={2}>
-          {landingTags.map((tag) => (
-            <UCategoryChip
-              key={tag.id}
-              label={tag.i18n?.[CommonUtils.parseAPII18nKey(lang)]?.name ?? ''}
-              active={activeCategoryId === tag.id}
-              onClick={() => setActiveCategoryId(tag.id ?? undefined)}
-            />
-          ))}
-        </UHStack>
+    <>
+      <CategoryProvider />
+      <LandingSectionWrapper
+        contentWrapperSx={{
+          paddingBottom: `${OVERLAPPED_SECTION_PADDING_BOTTOM}px`,
+        }}
+      >
+        <SectionTitleWithLink title="Articles" link={ROUTES.OPINION} />
+        <Stack gap={5}>
+          <UHStack gap={2}>
+            {landingTags.map((tag) => (
+              <UCategoryChip
+                key={tag.id}
+                label={
+                  tag.i18n?.[CommonUtils.parseAPII18nKey(lang)]?.name ?? ''
+                }
+                active={activeCategoryId === tag.id}
+                onClick={() => {
+                  if (activeCategoryId === tag.id) {
+                    setActiveCategoryId(undefined)
+                  } else {
+                    setActiveCategoryId(tag.id ?? undefined)
+                  }
+                }}
+              />
+            ))}
+          </UHStack>
 
-        {/** Posts */}
-        <OpinionPostCards opinions={opinions} pagination={false} />
-      </Stack>
-    </LandingSectionWrapper>
+          {/** Posts */}
+          <OpinionPostCards opinions={opinions} pagination={false} />
+        </Stack>
+      </LandingSectionWrapper>
+    </>
   )
 }
 
