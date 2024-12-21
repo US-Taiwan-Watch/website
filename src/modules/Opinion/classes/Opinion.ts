d19c6222e6c3367d91d38a79612b70a6861018c4
RandyLiu6410 <47877698+RandyLiu6410@users.noreply.github.com>
2024-12-21 15:22:24 +0800

    feat: Refactor opinion content serializer (#106)
    * feat: Refactor opinion content serializer
    
    * length check

--- src/modules/Opinion/classes/Opinion.ts
+++ src/modules/Opinion/classes/Opinion.ts
@@ -11,7 +11,7 @@ import { ROUTES } from '@/routes'
 import dayjs, { Dayjs } from 'dayjs'
 import { isArray, isObject, isString } from 'lodash-es'
 import { Article } from '@/common/lib/graphql/__generated__/graphql'
-import { slateToHtml, payloadSlateToHtmlConfig } from '@slate-serializers/html'
+import { Descendant } from '@/modules/Opinion/utils/slateSerializer'
 
 export type OpinionRepostSource = {
   title: string
@@ -44,7 +44,7 @@ export interface OpinionArgs {
   repostSources?: Array<OpinionRepostSource>
   thumbnailImage?: OpinionImage
   bannerImage?: OpinionImage
-  contentHtml?: string
+  content?: Array<Descendant>
   resources?: Array<OpinionResource>
   authors?: Array<OpinionAuthorArgs>
 }
@@ -60,7 +60,7 @@ export class Opinion {
   repostSources?: Array<OpinionRepostSource>
   thumbnailImage?: OpinionImage
   bannerImage?: OpinionImage
-  contentHtml?: string
+  content?: Array<Descendant>
   resources?: Array<OpinionResource>
   authors?: Array<OpinionAuthor>
 
@@ -97,8 +97,8 @@ export class Opinion {
     if (isObject(args.bannerImage)) {
       this.bannerImage = args.bannerImage
     }
-    if (isString(args.contentHtml)) {
-      this.contentHtml = args.contentHtml
+    if (isArray(args.content)) {
+      this.content = args.content
     }
     if (isArray(args.resources)) {
       this.resources = args.resources
@@ -139,13 +139,10 @@ export class Opinion {
       },
       authors: dto.authors?.map((author) => ({
         name: author.name,
-        descriptionHtml: author.bio,
+        description: author.bio,
       })),
-      contentHtml: slateToHtml(dto.content, {
-        ...payloadSlateToHtmlConfig,
-        convertLineBreakToBr: true,
-      }),
       description: dto.excerpt,
+      content: dto.content,
     })
   }
 
