de25526a4fe6a1a500c5be0519aa80330136f4d8
RandyLiu6410 <wert6410@gmail.com>
2025-01-02 23:31:12 +0800

    feat: Podcast related change

--- src/modules/Podcast/components/EpisodeCard.tsx
+++ src/modules/Podcast/components/EpisodeCard.tsx
@@ -197,7 +197,7 @@ const EpisodeCard = memo(
                 <Stack
                   direction="row"
                   spacing={1}
-                  alignItems="flex-end"
+                  alignItems="center"
                   justifyContent="center"
                   flex={1}
                 >
