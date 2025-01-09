c8c009eaeb8b038bfa4a73a35189241624eb7cd8
Zi-Hao Lin <49062749+I-am-ZiHao@users.noreply.github.com>
2025-01-09 22:21:32 +0800

    Static asset update (#126)
    * feat: ketagalan media data
    
    * feat: Bill landing page title and description
    
    * feat: podcast data
    
    * fix: minor fix
    
    ---------
    
    Co-authored-by: 林子皓 <>

--- src/modules/Bill/components/BillLanding/Introduction.tsx
+++ src/modules/Bill/components/BillLanding/Introduction.tsx
@@ -9,6 +9,10 @@ import { Stack, Typography, useTheme } from '@mui/material'
 import Link from 'next/link'
 import { useMemo } from 'react'
 
+const PAGE_TITLE = 'Update from this Congress'
+const PAGE_DESCRIPTION =
+  'The 118th U.S. Congress has actively advanced legislation to strengthen U.S.-Taiwan relations through key bills addressing security, economic ties, and international participation. The United States-Taiwan Initiative on 21st-Century Trade First Agreement Implementation Act (H.R. 4004) enhances economic cooperation through trade agreements, while the Taiwan Conflict Deterrence Act of 2023 (H.R. 554) seeks to deter aggression by targeting financial institutions linked to Chinese officials. The Taiwan International Solidarity Act (H.R. 1176) reinforces support for Taiwan’s participation in international organizations, countering Beijing’s diplomatic pressure. Additionally, the PROTECT Taiwan Act (H.R. 803) aims to deepen U.S.-Taiwan defense cooperation. Together, these legislative efforts reflect a bipartisan commitment to bolstering Taiwan’s security, international standing, and economic partnership with the United States amidst rising geopolitical tensions in the Indo-Pacific region.'
+
 const StyledBillTotalCountCard = styled(Stack)(({ theme }) => ({
   padding: theme.spacing(3, 4),
   justifyContent: 'center',
@@ -29,14 +33,8 @@ export default function Introduction() {
   return (
     <UHStack alignItems="center" justifyContent="space-between" width="100%">
       <Stack spacing={4} maxWidth="80%">
-        <Typography variant="h3">Updates from this Congress</Typography>
-        <Typography variant="bodyM">
-          Contrary to popular belief, Lorem Ipsum is not simply random text. It
-          has roots in a piece of classical Latin literature from 45 BC, making
-          it over 2000 years old. Richard McClintock, a Latin professor at
-          Hampden-Sydney College in Virginia, looked up one of the more obscure
-          Latin words, consectetur, from a Lorem Ipsum passage
-        </Typography>
+        <Typography variant="h3">{PAGE_TITLE}</Typography>
+        <Typography variant="bodyM">{PAGE_DESCRIPTION}</Typography>
       </Stack>
 
       <Link
