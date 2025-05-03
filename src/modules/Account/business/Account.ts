e67879392705a0cd4304cca2ef6d44727ac36856
RandyLiu6410 <47877698+RandyLiu6410@users.noreply.github.com>
2025-05-03 22:18:40 +0800

    feat: Update me gql (#174)

--- src/modules/Account/business/Account.ts
+++ src/modules/Account/business/Account.ts
@@ -1,4 +1,5 @@
 import { Me } from '@/common/lib/graphql/__generated__/graphql'
+import { Language } from '@/common/lib/i18n/types'
 import AccountSubscribeUtils, {
   accountSubscribeSchema,
   AccountSubscribeType,
@@ -7,6 +8,7 @@ import { ArticleType, ArticleUtils } from '@/modules/Article/business/Article'
 import { BillUtils } from '@/modules/Bill/business/Bill'
 import { PeopleUtils } from '@/modules/People/business/People'
 import { User } from '@auth0/nextjs-auth0/types'
+import { isNull } from 'lodash-es'
 import { z } from 'zod'
 
 const accountSchema = z.object({
@@ -30,55 +32,86 @@ export default class AccountUtils {
     return accountSchema.parse(input)
   }
 
-  static parseMeAndAuth0User(me: Me, user: User) {
+  static parseMeAndAuth0User(lang: Language, me: Me, user: User) {
     return AccountUtils.parse({
       id: user.sub ?? '',
       givenName: user.given_name ?? '',
       familyName: user.family_name ?? '',
       fullName: me.fullName ?? '',
       email: me.email ?? '',
-      subscribeBills: AccountUtils.parseSubscribeBills(me.subscribeBills),
-      subscribePeoples: AccountUtils.parseSubscribePeoples(me.subscribePeoples),
-      bookmarkArticles: AccountUtils.parseBookmarkArticles(me.bookmarkArticles),
+      subscribeBills: AccountUtils.parseSubscribeBills(lang, me.subscribeBills),
+      subscribePeoples: AccountUtils.parseSubscribePeoples(
+        lang,
+        me.subscribePeoples
+      ),
+      bookmarkArticles: AccountUtils.parseBookmarkArticles(
+        lang,
+        me.bookmarkArticles
+      ),
       notifications: me.notifications ?? [],
       picture: user.picture,
     })
   }
 
-  static parseSubscribeBills(subscribeBills: Me['subscribeBills']) {
+  static parseSubscribeBills(
+    lang: Language,
+    subscribeBills: Me['subscribeBills']
+  ) {
     if (!subscribeBills) return []
-    return subscribeBills.map((billId) => {
-      return AccountSubscribeUtils.parse({
-        id: billId ?? '',
-        type: AccountSubscribeType.Bill,
-        title: billId ?? '',
-        url: BillUtils.getLink(billId ?? ''),
+    return subscribeBills
+      .map((subscribeBill) => {
+        if (!subscribeBill) return null
+        const bill = BillUtils.parse(lang, subscribeBill)
+        return AccountSubscribeUtils.parse({
+          id: bill.id ?? '',
+          type: AccountSubscribeType.Bill,
+          title: bill.title ?? '',
+          url: BillUtils.getLink(bill.id),
+        })
       })
-    })
+      .filter((subscribe) => !isNull(subscribe))
   }
 
-  static parseSubscribePeoples(subscribePeoples: Me['subscribePeoples']) {
+  static parseSubscribePeoples(
+    lang: Language,
+    subscribePeoples: Me['subscribePeoples']
+  ) {
     if (!subscribePeoples) return []
-    return subscribePeoples.map((peopleId) => {
-      return AccountSubscribeUtils.parse({
-        id: peopleId ?? '',
-        type: AccountSubscribeType.People,
-        title: peopleId ?? '',
-        url: PeopleUtils.getLink(peopleId ?? ''),
+    return subscribePeoples
+      .map((subscribePeople) => {
+        if (!subscribePeople) return null
+        const people = PeopleUtils.parse(lang, subscribePeople)
+        return AccountSubscribeUtils.parse({
+          id: people.id ?? '',
+          type: AccountSubscribeType.People,
+          title: people.name ?? '',
+          url: PeopleUtils.getLink(people.id),
+        })
       })
-    })
+      .filter((subscribe) => !isNull(subscribe))
   }
 
-  static parseBookmarkArticles(bookmarkArticles: Me['bookmarkArticles']) {
+  static parseBookmarkArticles(
+    lang: Language,
+    bookmarkArticles: Me['bookmarkArticles']
+  ) {
     if (!bookmarkArticles) return []
-    return bookmarkArticles.map((articleId) => {
-      return AccountSubscribeUtils.parse({
-        id: articleId ?? '',
-        type: AccountSubscribeType.Article,
-        title: articleId ?? '',
-        url: ArticleUtils.getLink(ArticleType.Article, articleId ?? ''),
+    return bookmarkArticles
+      .map((subscribeArticle) => {
+        if (!subscribeArticle) return null
+        const article = ArticleUtils.parse(
+          lang,
+          subscribeArticle,
+          ArticleType.Article
+        )
+        return AccountSubscribeUtils.parse({
+          id: article.id ?? '',
+          type: AccountSubscribeType.Article,
+          title: article.title ?? '',
+          url: ArticleUtils.getLink(ArticleType.Article, article.id),
+        })
       })
-    })
+      .filter((subscribe) => !isNull(subscribe))
   }
 
   static getAccountSubscribeList(account: Account) {
