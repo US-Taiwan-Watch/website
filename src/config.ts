39c8f4e1c36226bd18a2be01d82193c8ac4aa12d
RandyLiu6410 <wert6410@gmail.com>
2025-05-03 13:47:51 +0800

    fix: env

--- src/config.ts
+++ src/config.ts
@@ -6,7 +6,6 @@ export const config = {
   GRAPHQL_API_URL: process.env.NEXT_PUBLIC_GRAPHQL_API_URL,
   AUTH0_DOMAIN: process.env.AUTH0_DOMAIN!,
   AUTH0_CLIENT_ID: process.env.AUTH0_CLIENT_ID!,
-  AUTH0_REDIRECT_URI: process.env.AUTH0_REDIRECT_URI!,
   AUTH0_CLIENT_SECRET: process.env.AUTH0_CLIENT_SECRET!,
   AUTH0_SECRET: process.env.AUTH0_SECRET!,
 } as const
