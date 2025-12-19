409b22009e0cee0d109bd0f8dfaf0273a4b57556
RandyLiu6410 <wert6410@gmail.com>
2025-12-20 00:05:37 +0800

    fix: ts

--- src/modules/About/Data/components/DataContent.tsx
+++ src/modules/About/Data/components/DataContent.tsx
@@ -6,5 +6,11 @@ import { CommonMdxComponents } from '@/modules/About/utils/mdxComponents'
 export default async function DataContent({ lang }: { lang: Language }) {
   const source = await getMarkdownAssetFile('Data', lang)
 
-  return <GMdxContentServer source={source} components={CommonMdxComponents} />
+  return (
+    <GMdxContentServer
+      source={source}
+      components={CommonMdxComponents}
+      lang={lang}
+    />
+  )
 }
