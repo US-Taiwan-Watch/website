21f8764693924c2e9ec23ed43ca96878a66782d6
RandyLiu6410 <wert6410@gmail.com>
2025-12-27 16:32:32 +0800

    fix: dayjs locale

--- src/modules/People/components/PeopleTracker/CardContent/Experience.tsx
+++ src/modules/People/components/PeopleTracker/CardContent/Experience.tsx
@@ -12,8 +12,6 @@ import UContentCard from '@/common/components/atoms/UContentCard'
 import { People, PeopleUtils } from '@/modules/People/business/People'
 import useTranslationClient from '@/common/lib/i18n/hooks/useTranslationClient'
 import { DateUtils } from '@/modules/Common/business/Date'
-import { useParams } from 'next/navigation'
-import { Language } from '@/common/lib/i18n/types'
 
 /**
  * 計算經歷的時間
@@ -236,8 +234,6 @@ interface ExperienceProps {
  */
 const Experience = function Experience({ experience }: ExperienceProps) {
   const { t } = useTranslationClient(['people'])
-  const { lang } = useParams<{ lang: Language }>()
-  DateUtils.setDayjsLocale(lang)
 
   return (
     <UContentCard
